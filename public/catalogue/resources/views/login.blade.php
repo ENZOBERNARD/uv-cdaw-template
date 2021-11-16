db4e5ca7753f3ab8985af53dc7597809468699ff
Mohammed Issam Kassel <kassel.issam@gmail.com>
2021-11-16 16:02:56 +0100

    Login controller

--- public/catalogue/resources/views/login.blade.php
+++ public/catalogue/resources/views/login.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="css/navbar.css" rel="stylesheet" />
+    <link href="../resources/css/navbar.css" rel="stylesheet" />
 
         <!-- Bootstrap CSS -->
 <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.2/css/bootstrap.min.css'>
